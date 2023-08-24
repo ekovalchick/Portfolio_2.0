40192d4af36ad3842126c9ea466871400448d459
Elizabeth Kovalchick <ekovalchick97@gmail.com>
2023-08-23 22:30:48 -0400

    update lava

--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,111 @@
-// lava code start
-const app = {
-data() {
-    return {
-      circle1: { x: 400, y: 200, xSpeed: 0.2, ySpeed: 0.34 },
-      circle2: { x: 600, y: 400, xSpeed: -0.5, ySpeed: 0.5 },
-      circle3: { x: 800, y: 600, xSpeed: 0.3, ySpeed: 0.6 },
-      circle4: { x: 1000, y: 500, xSpeed: -0.1, ySpeed: 0.6 },
-      circle5: { x: 900, y: 300, xSpeed: -0.2, ySpeed: 0.3 },
-      bounds: {
-        minX: -150,
-        minY: -150,
-        maxX: window.innerWidth + 100,
-        maxY: window.innerHeight + 100,
-      },
-    };
-  },
-  mounted() {
-    setInterval(() => {
-      this.updateCirclePosition(this.circle1);
-      this.updateCirclePosition(this.circle2);
-      this.updateCirclePosition(this.circle3);
-      this.updateCirclePosition(this.circle4);
-      this.updateCirclePosition(this.circle5);
-    }, 10);
-  },
-  methods: {
-    updateCirclePosition(circle) {
-      circle.x += circle.xSpeed;
-      circle.y += circle.ySpeed;
-      if (circle.x < this.bounds.minX) {
-        circle.x = this.bounds.minX + Math.random() * 10;
-        circle.xSpeed = Math.abs(circle.xSpeed);
-      } else if (circle.x > this.bounds.maxX) {
-        circle.x = this.bounds.maxX - Math.random() * 10;
-        circle.xSpeed = -Math.abs(circle.xSpeed);
-      }
-      if (circle.y < this.bounds.minY) {
-        circle.y = this.bounds.minY + Math.random() * 10;
-        circle.ySpeed = Math.abs(circle.ySpeed);
-      } else if (circle.y > this.bounds.maxY) {
-        circle.y = this.bounds.maxY - Math.random() * 10;
-        circle.ySpeed = -Math.abs(circle.ySpeed);
-      }
-    },
-  }
+const canvas = document.getElementById('canvas1');
+
+const ctx= canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.fillStyle = '#CE547E'
+
+//object oriented programming below
+
+class Ball{
+    //will define the movement and what it looks like
+    constructor(effect){
+        this.effect = effect;
+        this.x = this.effect.width * 0.5;
+        this.y = this.effect.height * 0.5;
+        // x & ymakes the starting point in the middle of page
+        this.radius = Math.random() * 90 + 20;
+        this.speedX = Math.random() - 0.5;
+        this.speedY = Math.random() - 0.5;
+        //speed positive = right movement(x) positive direction (y)
 
 
-};
+    }
+    update(){
+        //make the metaBalls bounce
+        if (this.x < this.radius || this.x > this.effect.width - this.radius) this.speedX *= -1;
+        if (this.y < this.radius || this.y > this.effect.height - this.radius) this.speedY *= -1;
 
-Vue.createApp(app).mount('#app')
+        this.x += this.speedX
+        this.y += this.speedY
+        //makes the balls move 
+    }
+    draw(context){
+        //this describes what canvas to draw on
+        context.beginPath();
+        //creates a new circle shape
+        context.arc(this.x,this.y,this.radius,0, Math.PI *2);
+        //0=start angle where to draw, end angle
+        context.fill()
+        ///can change this to a diff color with fill
+    }
+    reset(){
+        this.x = this.effect.width * 0.5;
+        this.y = this.effect.height * 0.5;
+        //when webpage is resized, lava is responsive
+    }
 
-//lava code end
+}
+
+class MetaballsEffect{
+//manage effects.
+    constructor(width, height){
+        this.width = width;
+        this.height = height;
+        this.metaballsArray = [];
+        //will store all metaballs created
+    }
+    init(numberOfBalls){
+        //initialize 
+        for (let i = 0; i < numberOfBalls; i++) {
+            this.metaballsArray.push(new Ball(this))
+            
+        }
+
+    }
+    update(){
+        this.metaballsArray.forEach(metaball => metaball.update())
+        //uses update method in line 23 (moves ball)
+    }
+    draw(context){
+        this.metaballsArray.forEach(metaball => metaball.draw(context))
+        //uses draw from 28
+
+
+    }
+    reset(newWidth, newHeight){
+        this.width = newWidth;
+        this.height = newHeight;
+        this.metaballsArray.forEach(metaball => metaball.reset());
+        //responsive metaballs 
+    }
+
+}
+
+const effect = new MetaballsEffect(canvas.width, canvas.height)
+//instant of this class want the effect to be full screen so use width and height 
+
+effect.init(20)
+//because line 48 is expecting #of balls input
+
+console.log(effect)
+
+function animate(){
+    //will run loop to update and draw the metaballs
+    ctx.clearRect(0,0,canvas.width, canvas.height)
+    //need to clear previous animation frames
+    effect.update()
+    effect.draw(ctx)
+    //expects content so use ctx
+    requestAnimationFrame(animate)
+
+}
+animate()
+
+window.addEventListener('resize', function(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx.fillStyle = '#CE547E';
+    //^so will keep the metaballs when moving webpage size
+    effect.reset(canvas.width, canvas.height)
+})
