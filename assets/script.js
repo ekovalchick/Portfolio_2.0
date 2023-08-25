fdd89cb3ffce03f53e5d418e7f8f1bb3cc9b1f18
Elizabeth Kovalchick <ekovalchick97@gmail.com>
2023-08-25 15:51:34 -0400

    udpate nav bar and quick links homepage

--- assets/script.js
+++ assets/script.js
@@ -122,14 +122,14 @@ window.addEventListener('resize', function(){
 
 //navbar 
 
-window.onscroll = function () {scrollFunction();};
+// window.onscroll = function () {scrollFunction();};
 
-function scrollFunction() {
-if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+// function scrollFunction() {
+// if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
 
-document.getElementById("navbar").style.background = "#501e27";
-} else {
+// document.getElementById("navbar").style.background = "#501e27";
+// } else {
 
-document.getElementById("navbar").style.background = "none";
-}
-}
+// document.getElementById("navbar").style.background = "none";
+// }
+// }
